74d5875b45bfb9dba2dfca4f43946ffd437de505
Shoaib3375 <mestabd60@gmail.com>
2022-06-12 17:40:48 +0600

    change prime number

--- PHP/Class1/Exercises/primeorNot.php
+++ PHP/Class1/Exercises/primeorNot.php
@@ -1,12 +1,8 @@
 <?php
 function check_prime( $num )
 {
-    $int = intval($num);
-
     if ( $num == 1 ) {
         return 0;
-    }elseif ($num<$int || $num > $int) {
-        return 2;
     }
     for ( $i = 2; $i <= $num / 2; $i++ ) {
         if ( $num % $i == 0 ) {
@@ -17,14 +13,14 @@ function check_prime( $num )
 }
 
 $num = readline( "Enter a positive number: " );
+
 $flag_val = check_prime( $num );
- if ( $num == 0 || $num == 1 ) {
+if ( $num < 0 ) {
+    echo "Enter a positive integer Number";
+} else if ( $num == 0 || $num == 1 ) {
     echo "$num is not a prime number";
 } else if ( $flag_val == 0 ) {
     echo "$num is not a prime number";
-}elseif ($flag_val == 2) {
-    echo "Enter a positive value";
-}
- else {
+} else {
     echo "$num is a prime number";
 }
