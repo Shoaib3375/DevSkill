90870854d3c21ed26f0ecb225c9b96b60a74f62f
Md Shoaib Islam <mestabd60@gmail.com>
2022-06-12 18:03:36 +0600

    palindrome done

--- PHP/Class1/Exercises/palindrome.php
+++ PHP/Class1/Exercises/palindrome.php
@@ -2,13 +2,13 @@
 // Palindrome Done
 for ( $i = 100; $i < 10000; $i++ ) {
     $n = $i;
-    $rev = 0;
+    $reverse = 0;
     while ( $n > 0 ) {
-        $rem = $n % 10;
-        $rev = ( $rev * 10 ) + $rem;
-        $n = ( $n - $rem ) / 10;
+        $remainder = $n % 10;
+        $reverse = ( $reverse * 10 ) + $remainder;
+        $n = ( $n - $remainder ) / 10;
     }
-    if ( $i == $rev ) {
-        echo $i . " is a Palindrome number";
+    if ( $i == $reverse ) {
+        echo $i . " is a Palindrome number \n";
     }
 }
